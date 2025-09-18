a4130fe6b550a1e17bf1e1bc387d4c2f3897d435
Melanie Rodriguez <melarodrigg@gmail.com>
2025-09-18 01:05:18 -0600

    Attempt 1

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+require('datejs');
 
+//Create a function that merges all usernames into one.
 
+const usernames1 = ["melang", "calderow", "dzumbado"]
+const usernames2 = ["merodriguez", "wisoto", "dischultz"]
+
+function combineUsers (...args) {
+  let combineObject = {
+    users: [],
+  };
+  //Loop through args ??
+  combineObject.users = [].concat(...args)
+  combineObject.merge_date = new Date().toString("M/d/yyyy");
+  return combineObject
+};
+
+console.log(combineUsers(usernames1, usernames2));
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
